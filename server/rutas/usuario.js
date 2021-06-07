ef85b8e286e5b11f0500bb932782f1bb73c526f6
unknown <r.loboplaza@hotmail.com>
2021-06-07 09:59:45 -0300

    agregando rutas get, post y put

--- server/rutas/usuario.js
+++ server/rutas/usuario.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
 const Usuario = require('../modelos/usuario');
 const app = express();
 
 app.get('/usuarios', function (req, res) {
-  res.json('GET usuario');
+  let desde = req.query.desde || 0;
+  desde = Number(desde);
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+  // res.json('GET usuario');
+  Usuario.find({ estado: true })
+    .skip(desde)
+    .limit(limite)
+    .exec((err, usuarios) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+
+      Usuario.count({}, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          cantidad: conteo,
+        });
+      });
+    });
 });
 
 app.post('/usuarios', function (req, res) {
@@ -28,24 +53,29 @@ app.post('/usuarios', function (req, res) {
       usuario: usuarioDB,
     });
   });
-
-  // if (body.usuario === undefined) {
-  //   res.status(400).json({
-  //     ok: false,
-  //     message: 'El usuario es necesario',
-  //   });
-  // } else {
-  //   res.json({
-  //     body,
-  //   });
-  // }
 });
 
-app.put('/usuarios:id', function (req, res) {
-  res.json('PUT usuario');
+app.put('/usuarios/:id', function (req, res) {
+  // res.json('PUT usuario');
+  let body = _.pick(req.body, ['nombre', 'img', 'role', 'estado']);
+  let id = req.params.id;
+
+  Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
+    });
+  });
 });
 
-app.delete('/usuarios:id', function (req, res) {
+app.delete('/usuarios/:id', function (req, res) {
   res.json('DELETE usuario');
 });
 module.exports = app;
